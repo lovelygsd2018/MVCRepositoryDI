6552ecf72047cb015d03e0d095b3446980a1eefd
lovelygsd2018 <44107410+lovelygsd2018@users.noreply.github.com>
2018-10-17 08:10:43 -0500

    Create IRepository.cs

---
+++ Repository/IRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_DI_2.Repository
+{
+    public interface IRepository<T> 
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+    }
+}
