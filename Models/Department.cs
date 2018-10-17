a94d78a6171e930160df2bfad8755449b473b809
lovelygsd2018 <44107410+lovelygsd2018@users.noreply.github.com>
2018-10-17 08:07:05 -0500

    Create Department.cs

---
+++ Models/Department.cs
@@ -0,0 +1,11 @@
+namespace Emp_DI_2.Models
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class Department
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
